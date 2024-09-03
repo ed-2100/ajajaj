95a515c2abe28e10d80653ba8ef1f6245b5c0b0a
Edwin Burwell <edwinburwell758@gmail.com>
2024-09-03 09:49:41 -0500

    Commit 4.

--- src/main.rs
+++ src/main.rs
@@ -5,8 +5,10 @@
 #[used]
 pub static BOOT_LOADER: [u8; 256] = rp2040_boot2::BOOT_LOADER_W25Q080;
 
+use alloc::format;
 use core::mem::MaybeUninit;
 use embassy_executor::{Executor, InterruptExecutor};
+use embassy_rp::i2c_slave::ReadStatus;
 use embassy_rp::interrupt;
 use embassy_rp::{
     bind_interrupts,
@@ -16,11 +18,19 @@ use embassy_rp::{
     peripherals::{I2C1, UART0},
     uart,
 };
-use embassy_time::{Duration, Instant, Ticker, Timer};
+use embassy_time::{Duration, Instant, Ticker, Timer, TICK_HZ};
+
 use fixed::traits::ToFixed;
-use fixed::types::{U0F16, U16F16, U1F15, U28F4};
+use fixed::types::{U16F16, U28F4};
 use panic_probe as _;
-use rtt_target::rprint;
+use rtt_target::{rprint, rprintln};
+
+// #[no_mangle]
+// static RTIC_ASYNC_MAX_LOGICAL_PRIO: u8 = 3; // 1 << embassy_rp::NVIC_PRIO_BITS;
+
+// use rtic_monotonics::rp2040::prelude::*;
+
+// rp2040_timer_monotonic!(Mono);
 
 #[allow(unused_imports)]
 use num_traits::*;
@@ -37,19 +47,19 @@ bind_interrupts!(struct Irqs {
     UART0_IRQ => uart::InterruptHandler<UART0>;
 });
 
-static EXECUTOR_HIGH: InterruptExecutor = InterruptExecutor::new();
+// static EXECUTOR_HIGH: InterruptExecutor = InterruptExecutor::new();
 static EXECUTOR_MED: InterruptExecutor = InterruptExecutor::new();
 static mut EXECUTOR_LOW: MaybeUninit<Executor> = MaybeUninit::uninit();
 
 // static CHANNEL: Channel<CriticalSectionRawMutex, Box<str>, 64> = Channel::new();
 
-#[cortex_m_rt::interrupt]
-unsafe fn SWI_IRQ_0() {
-    EXECUTOR_HIGH.on_interrupt()
-}
+// #[cortex_m_rt::interrupt]
+// unsafe fn SWI_IRQ_0() {
+//     EXECUTOR_HIGH.on_interrupt()
+// }
 
 #[cortex_m_rt::interrupt]
-unsafe fn SWI_IRQ_1() {
+unsafe fn SWI_IRQ_0() {
     EXECUTOR_MED.on_interrupt()
 }
 
@@ -64,6 +74,13 @@ fn main() -> ! {
         unsafe { HEAP.init(HEAP_MEM.as_ptr() as usize, HEAP_SIZE) }
     }
 
+    // unsafe {
+    //     Mono::start(
+    //         rtic_monotonics::rp2040::TIMER::steal(),
+    //         &rtic_monotonics::rp2040::RESETS::steal(),
+    //     );
+    // }
+
     let i2c = {
         let mut config = embassy_rp::i2c_slave::Config::default();
         config.addr = 0x17;
@@ -71,26 +88,31 @@ fn main() -> ! {
         embassy_rp::i2c_slave::I2cSlave::new(p.I2C1, p.PIN_3, p.PIN_2, Irqs, config)
     };
 
-    let uart = uart::Uart::new(
-        p.UART0,
-        p.PIN_0,
-        p.PIN_1,
-        Irqs,
-        p.DMA_CH1,
-        p.DMA_CH0,
-        Default::default(),
-    );
+    // let uart = uart::Uart::new(
+    //     p.UART0,
+    //     p.PIN_0,
+    //     p.PIN_1,
+    //     Irqs,
+    //     p.DMA_CH1,
+    //     p.DMA_CH0,
+    //     Default::default(),
+    // );
 
     rtt_target::rtt_init_print!();
 
-    let (tx, rx) = uart.split();
+    // let (tx, rx) = uart.split();
 
-    interrupt::SWI_IRQ_0.set_priority(Priority::P2);
-    let high_spawner = EXECUTOR_HIGH.start(interrupt::SWI_IRQ_0);
+    // interrupt::SWI_IRQ_0.set_priority(Priority::P1);
+    // let high_spawner = EXECUTOR_HIGH.start(interrupt::SWI_IRQ_0);
     // spawner.spawn(run_high()).unwrap();
+    // interrupt::I2C0_IRQ.set_priority(Priority::P1);
+    // interrupt::TIMER_IRQ_0.set_priority(Priority::P2);
+    // interrupt::TIMER_IRQ_1.set_priority(Priority::P2);
+    // interrupt::TIMER_IRQ_2.set_priority(Priority::P2);
+    // interrupt::TIMER_IRQ_3.set_priority(Priority::P2);
 
-    interrupt::SWI_IRQ_1.set_priority(Priority::P3);
-    let med_spawner = EXECUTOR_MED.start(interrupt::SWI_IRQ_1);
+    interrupt::SWI_IRQ_0.set_priority(Priority::P3);
+    let med_spawner = EXECUTOR_MED.start(interrupt::SWI_IRQ_0);
     med_spawner
         .spawn(on_i2c_event(Output::new(p.PIN_16, Level::High), i2c))
         .unwrap();
@@ -117,69 +139,101 @@ async fn heartbeat(
     pin: impl embassy_rp::gpio::Pin,
     // sender: Sender<'static, CriticalSectionRawMutex, Box<str>, 64>,
 ) {
-    let mut ticker = Ticker::every(Duration::from_secs(1));
     let mut led = Output::new(pin, Level::Low);
 
+    let mut ticker = Ticker::every(Duration::from_secs(1));
     for i in 1.. {
-        ticker.next().await;
-
         led.toggle();
-        // sender.send(format!("Your count is: {}\n", i).into()).await;
-        let mut config = embassy_rp::pwm::Config::default();
-        config.phase_correct = true;
+        // let mut config = embassy_rp::pwm::Config::default();
+        // config.phase_correct = true;
 
-        let start = Instant::now();
-        config = set_freq_duty(config, 3000.to_fixed(), 0.75.to_fixed(), 0.25.to_fixed());
-        let duration = Instant::now() - start;
+        // let duration = {
+        //     // let start = Mono::now();
+        //     let start = Instant::now();
+        //     set_freq_duty(
+        //         &mut config,
+        //         3.75.to_fixed(),
+        //         0.75.to_fixed(),
+        //         0.25.to_fixed(),
+        //     )
+        //     .unwrap();
+        //     // Mono::now() - start
+        //     Instant::now() - start
+        // };
+
+        // block_for(Duration::from_millis(100));
+
+        // sender.send(format!("Your count is: {}\n", i).into()).await;
         rprint!(
-            "{}, {}, {}, {}, {}, {}, {}, {}\n",
+            "{}\n",
             i,
-            config.top,
-            config.divider,
-            config.compare_a,
-            (embassy_rp::clocks::clk_sys_freq() as f32)
-                / ((if config.phase_correct { 2f32 } else { 1f32 })
-                    * ((config.top + 1) as f32)
-                    * config.divider.to_num::<f32>()),
-            (config.compare_a as f32) / ((config.top + 1) as f32),
-            (config.compare_b as f32) / ((config.top + 1) as f32),
-            duration.as_micros()
+            // config.top,
+            // config.divider,
+            // config.compare_a,
+            // config.compare_b,
+            // (embassy_rp::clocks::clk_sys_freq() as f32)
+            //     / ((if config.phase_correct { 2f32 } else { 1f32 })
+            //         * ((config.top + 1) as f32)
+            //         * config.divider.to_num::<f32>()),
+            // (config.compare_a as f32) / ((config.top + 1) as f32),
+            // (config.compare_b as f32) / ((config.top + 1) as f32),
+            // interrupt::I2C1_IRQ.get_priority(),
+            // interrupt::TIMER_IRQ_0.get_priority(),
+            // duration.as_micros()
         );
+
+        // Mono::delay(1.secs()).await;
+        ticker.next().await;
     }
 }
 
+const fn top_duty_to_comp(top: u16, duty: U16F16) -> u16 {
+    ((((top + 1) as u32) * duty.to_bits()) >> 16) as _
+}
+
+/// Configures `top`, `divider`, `compare_a`, and `compare_b` given frequency and duty cycles.
+/// # Arguments:
+/// * `f_pwm`: from `sys_clk / (65535 * (256 + 15 / 16) * (config.phase_correct + 1))` to `sys_clk / 2`
+/// * `duty_a`: from `0.0` to `1.0`
+/// * `duty_b`: from `0.0` to `1.0`
 fn set_freq_duty(
-    mut config: embassy_rp::pwm::Config,
+    config: &mut embassy_rp::pwm::Config,
     f_pwm: U28F4,
-    duty_a: U1F15,
-    duty_b: U1F15,
-) -> embassy_rp::pwm::Config {
-    let f_pwm = f_pwm.to_bits();
+    duty_a: U16F16,
+    duty_b: U16F16,
+) -> Result<(), alloc::string::String> {
+    let f_pwm_bits = f_pwm.to_bits();
     let f_sys = embassy_rp::clocks::clk_sys_freq();
 
-    let div = (f_sys as u64) << if config.phase_correct { 7 } else { 8 };
-    let mut div = (div / (65535 * f_pwm) as u64) as u32;
+    let mut div = (((f_sys as u64) << if config.phase_correct { 7 } else { 8 })
+        / (65535 * f_pwm_bits as u64)) as u32;
+
     if div < 1 << 4 {
         div = 1 << 4;
     }
 
-    assert!(
-        div <= 0xFFF,
-        "Too low of a frequency! Lower sys_clk, or raise the frequency! {:?}",
-        div
-    );
+    if div > 0xFFF {
+        return Err(format!("{}Hz is too low of a frequency for a sys_clk of {}Hz. Raise sys_clk, or lower the frequency!", f_pwm, f_sys));
+    }
 
-    let top = (f_sys as u64) << if config.phase_correct { 7 } else { 8 };
-    let mut top = (top / (div * f_pwm) as u64) as u32 - 1;
+    let mut top = ((((f_sys as u64) << if config.phase_correct { 7 } else { 8 })
+        / (div as u64 * f_pwm_bits as u64)) as u32)
+        .saturating_sub(1);
+
+    if top == 0 {
+        return Err(format!("{}Hz is too high of a frequency for a sys_clk of {}Hz. Raise sys_clk, or lower the frequency!", f_pwm, f_sys));
+    }
+
     if top > 65534 {
         top = 65534;
     }
 
-    config.top = top as _;
-    config.divider = fixed::FixedU16::<fixed::types::extra::U4>::from_bits(div as _);
-    config.compare_a = ((((config.top + 1) as u32) * (duty_a.to_bits() as u32)) >> 15) as _;
-    config.compare_b = ((((config.top + 1) as u32) * (duty_b.to_bits() as u32)) >> 15) as _;
-    config
+    config.top = top as u16;
+    config.divider = fixed::FixedU16::<fixed::types::extra::U4>::from_bits(div as u16);
+    config.compare_a = top_duty_to_comp(config.top, duty_a);
+    config.compare_b = top_duty_to_comp(config.top, duty_b);
+
+    Ok(())
 }
 
 #[embassy_executor::task]
@@ -191,13 +245,29 @@ async fn led_dimmer(
 ) {
     let mut config = embassy_rp::pwm::Config::default();
     config.phase_correct = true;
-    config = set_freq_duty(config, 3.75.to_fixed(), 0.75.to_fixed(), 0.25.to_fixed());
+    set_freq_duty(
+        &mut config,
+        600.to_fixed(),
+        0.75.to_fixed(),
+        0.25.to_fixed(),
+    )
+    .unwrap();
 
-    Timer::after_secs(2).await;
-    let _led = embassy_rp::pwm::Pwm::new_output_ab(slice, pina, pinb, config);
+    // Mono::delay(1.secs()).await;
+    Timer::after(Duration::from_secs(1)).await;
+    let mut led = embassy_rp::pwm::Pwm::new_output_ab(slice, pina, pinb, config.clone());
 
+    use core::f32::consts::TAU;
+
+    let mut ticker = Ticker::every(Duration::from_hz(30));
     loop {
-        Timer::after_secs(10).await;
+        // let x = Mono::now().ticks() as f32 / 1_000_000 as f32 * TAU;
+        let x = (Instant::now().as_ticks() % TICK_HZ) as f32 / TICK_HZ as f32 * TAU;
+        config.compare_a = top_duty_to_comp(config.top, ((x.sin() + 1.) / 2.).to_fixed());
+        config.compare_b = top_duty_to_comp(config.top, ((x.cos() + 1.) / 2.).to_fixed());
+        led.set_config(&config);
+        // Mono::delay((1_000_000 / 30).micros()).await;
+        ticker.next().await;
     }
 }
 
@@ -214,39 +284,47 @@ async fn led_dimmer(
 
 #[embassy_executor::task]
 async fn on_i2c_event(mut led: Output<'static>, mut i2c: i2c_slave::I2cSlave<'static, I2C1>) {
-    loop {
-        let mut buf = [0u8; 128];
+    let mut buf = [0u8; 128];
 
+    loop {
         match i2c.listen(&mut buf).await {
             Ok(i2c_slave::Command::GeneralCall(_)) => {}
             Ok(i2c_slave::Command::Read) => loop {
-                match i2c.respond_to_read(&[led.get_output_level() as u8]).await {
-                    Ok(x) => match x {
-                        i2c_slave::ReadStatus::Done => break,
-                        i2c_slave::ReadStatus::NeedMoreBytes => {}
-                        i2c_slave::ReadStatus::LeftoverBytes(_) => break,
-                    },
-                    Err(_) => {}
-                }
-            },
-            Ok(i2c_slave::Command::Write(len)) => led.set_level((buf[len - 1] != 0).into()),
-            Ok(i2c_slave::Command::WriteRead(len)) => loop {
                 match i2c
                     .respond_and_fill(&[led.get_output_level() as u8], 0)
                     .await
                 {
-                    Ok(x) => {
-                        led.set_level((buf[len - 1] != 0).into());
-
-                        match x {
-                            i2c_slave::ReadStatus::Done => break,
-                            i2c_slave::ReadStatus::NeedMoreBytes => {}
-                            i2c_slave::ReadStatus::LeftoverBytes(_) => break,
-                        }
+                    Ok(ReadStatus::NeedMoreBytes) => {}
+                    Ok(_) => break,
+                    Err(_) => {
+                        rprint!("error on read")
                     }
-                    Err(_) => {}
                 }
             },
+            Ok(i2c_slave::Command::Write(len)) => {
+                if len > 0 {
+                    led.set_level((buf[len - 1] != 0).into())
+                }
+            }
+            Ok(i2c_slave::Command::WriteRead(len)) => {
+                loop {
+                    rprintln!("*");
+                    match i2c
+                        .respond_and_fill(&[led.get_output_level() as u8], 0)
+                        .await
+                    {
+                        Ok(ReadStatus::NeedMoreBytes) => {}
+                        Ok(_) => break,
+                        Err(_) => {
+                            rprint!("error on writeread")
+                        }
+                    }
+                }
+
+                if len > 0 {
+                    led.set_level((buf[len - 1] != 0).into());
+                }
+            }
             Err(_) => {}
         }
     }
